b751c9e4b931b0fd061febcc9bd477f688fc8a33
Martine Yemma <70291070+mylaconcepts@users.noreply.github.com>
2021-03-08 20:18:19 -0800

    added CSON loader
    added CSON loader to webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
+module: {
+    rules: [
+            {
+        test: '/\.cson$/',
+        loader: "cson-loader"
+            }
+    ]
+}
